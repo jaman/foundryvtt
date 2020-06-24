6859d1b876c40faaade0bad746c3c23d90d50800
Jarius Jenkins <jarius.jenkins@gmail.com>
2020-06-24 20:03:58 +0900

    New version

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -3,8 +3,8 @@
 # All rights reserved
 
 # Variables.
-INSTALL_FILES=https://nextcloud.fithwum.tech/index.php/s/35GF6g2ro97Etne/download
-FVTT_VERSION=0.6.2
+INSTALL_FILES=https://foundryvtt.com/releases/download?version=0.6.4&platform=linux
+FVTT_VERSION=0.6.4
 
 echo " "
 echo "Server version is ${FVTT_VERSION}."
@@ -13,13 +13,13 @@ echo " "
 mkdir -p /foundry/fvtt /foundry/data
 #echo "Downloading and installing FoundryVTT-${FVTT_VERSION}."
 #echo " "
-#wget --no-cache ${INSTALL_FILES} -O /ftemp/foundryvtt-${FVTT_VERSION}.zip
+wget --no-cache ${INSTALL_FILES} -O /ftemp/foundryvtt-${FVTT_VERSION}.zip
 #sleep 1
-#unzip -d /foundry/fvtt/ /ftemp/foundryvtt-${FVTT_VERSION}.zip
+unzip -d /foundry/fvtt/ /ftemp/foundryvtt-${FVTT_VERSION}.zip
 #sleep 1
-#cp -uR /ftemp/fvtt/. /foundry/fvtt
-#rm -fr /ftemp/fvtt
-#rm -fr /ftemp/foundryvtt-${FVTT_VERSION}.zip
+cp -uR /ftemp/fvtt/. /foundry/fvtt
+rm -fr /ftemp/fvtt
+rm -fr /ftemp/foundryvtt-${FVTT_VERSION}.zip
 
 sleep 1
 
